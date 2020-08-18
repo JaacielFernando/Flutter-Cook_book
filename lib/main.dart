4e952b7ab13a387f5523c60e8e7057161a58448a
Fernando Maldonado <fernando@varopago.com>
2020-08-18 15:58:17 -0500

    Desing/Tab-Bar-Example

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:cook_book_flutter/src/pages/Animation_P1.dart';
 import 'package:cook_book_flutter/src/pages/home_page.dart';
 import 'package:cook_book_flutter/src/pages/snack_bar_page.dart';
 import 'package:flutter/material.dart';
+
+import 'src/pages/tabs_example_page.dart';
  
 void main() => runApp(MyApp());
  
@@ -15,6 +17,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => HomePage(),
         '/AnimationP1': (context) => AnimationP1(),
         '/SnackBarExample': (context) => SnackBarPage(),
+        '/TabBarExample': (context)=> TabBarDemo()
       }, 
     );
   }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -75,6 +75,30 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  InkWell(
+                    child: Text(
+                      'Tab Bar Example Page                   ',
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 18,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.pushNamed(context, '/TabBarExample');
+                    },
+                  ),
+                  RaisedButton(
+                    child: Text('Launch '),
+                    onPressed: () {
+                      // Navigate to the second screen using a named route.
+                      Navigator.pushNamed(context, '/TabBarExample');
+                    },
+                  ),
+                ],
+              ),
             ],
           ),
         ),
